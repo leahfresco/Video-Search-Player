0659907ec448d7eb64d477e6da76411b089303f7
Leah Yukelson <Leah Yukelson>
2017-07-26 18:26:45 -0700

    Add Youtube API and component files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAvSM9Kkqeg1R4G9XhHxpRVl37Oyrf9WfE';
+
 // Create a new component. This component should produce some HTML
-
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
-
 
 // Take component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
