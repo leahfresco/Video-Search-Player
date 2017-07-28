dcba25a1f47d2258a5bc1b0e3ad62d6f7e158802
Leah Yukelson <Leah Yukelson>
2017-07-27 18:16:38 -0700

    Add SearchBar class

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAvSM9Kkqeg1R4G9XhHxpRVl37Oyrf9WfE';
+
 
 // Create a new component. This component should produce some HTML
 const App = () => {
