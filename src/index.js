8980ebd3db910e3b2a157788fc1229336e41d184
Leah Yukelson <Leah Yukelson>
2017-07-31 15:56:28 -0700

    Added list of videos with first video embedded

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React , { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAvSM9Kkqeg1R4G9XhHxpRVl37Oyrf9WfE';
 
+class App extends Component {
+    constructor (props){
+        super(props);
 
-// Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboard'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render () {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
-// Take component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
