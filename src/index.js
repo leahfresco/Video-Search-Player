7ea7b5383d4b8f492611bf7c8f00a350aaa5f292
Leah Yukelson <Leah Yukelson>
2017-07-31 16:54:10 -0700

    Throttle searches with lodash

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import _ from 'lodash';
 import React , { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-
 const API_KEY = 'AIzaSyAvSM9Kkqeg1R4G9XhHxpRVl37Oyrf9WfE';
 
 class App extends Component {
@@ -16,7 +16,11 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({key: API_KEY, term: 'surfboard'}, (videos) => {
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({ 
                 videos: videos,
                 selectedVideo: videos[0]
@@ -25,9 +29,11 @@ class App extends Component {
     }
 
     render () {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
